afb34ef8e50cc58a7b166477981ea6b2d11200f0
Godó Norbert <godikensei@gmail.com>
2018-05-20 22:47:01 +0200

    Character is moving in the scene with idle and walking animation.

---
+++ Norben-1/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    private CharacterController controller;
+
+    public float walkingSpeed = 2;
+    public float jumpForce = 7;
+    public float gravity = -12;
+    float currentSpeed;
+    float velocityY;
+
+    Vector3 movement;
+    Animator anim;
+    Rigidbody playerRigidbody;
+
+    public float turnSmoothTime = 0.1f;
+    float turnSmoothVelocity;
+
+    int floorMask;
+    float camRayLength = 100f;
+
+    Transform cameraT;
+
+    // Use this for initialization
+    void Awake () {
+        anim = GetComponent<Animator>();
+        //playerRigidbody = GetComponent<Rigidbody>();
+        controller = GetComponent<CharacterController>();
+
+        cameraT = Camera.main.transform;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+
+
+        if(h != 0f || v != 0f)
+        {
+            currentSpeed = walkingSpeed;
+        }
+        else if(h == 0f || v == 0f)
+        {
+            currentSpeed = 0f;
+        }
+        transform.Translate(transform.forward * currentSpeed * Time.deltaTime, Space.World);
+        //Vector3 velocity = transform.forward * currentSpeed;
+
+        //controller.Move(velocity * Time.deltaTime);
+
+        //velocityY += Time.deltaTime * gravity;
+
+        //Move(h, v);
+        Animating(h, v);
+        Turning(h, v);
+
+    }
+
+   /* private void Jump()
+    {
+        if (controller.isGrounded)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                velocityY = jumpForce;
+            }
+
+        } else
+        {
+            velocityY -= gravity * Time.deltaTime;
+        }
+
+        Vector3 moveVector = new Vector3(0, velocityY, 0);
+        controller.Move(moveVector * Time.deltaTime);
+    }*/
+
+    void Move(float h, float v)
+    {
+        movement.Set(h, 0f, v);
+        //A h és v-t ha egyszerre nyomjuk, akkor 1.4 lesz, ezért normalizálni kell
+        movement = movement.normalized * walkingSpeed * Time.deltaTime;
+
+        playerRigidbody.MovePosition(transform.position + movement);
+    }
+
+    void Turning(float h , float v)
+    {
+        Vector2 input = new Vector2(h, v);
+        Vector2 inputDir = input.normalized;
+
+        if(inputDir != Vector2.zero)
+        {
+            float targetRotation = Mathf.Atan2(inputDir.x, inputDir.y) * Mathf.Rad2Deg + cameraT.eulerAngles.y;
+            transform.eulerAngles = Vector3.up * Mathf.SmoothDampAngle(transform.eulerAngles.y, targetRotation, ref turnSmoothVelocity, turnSmoothTime);
+        }
+    }
+
+    void Animating(float h, float v)
+    {
+        bool walking = h != 0f || v != 0f;
+
+        anim.SetBool("IsWalking", walking);
+    }
+
+}
---
+++ Norben-1/Assets/Scripts/ThirdPersonCamera.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonCamera : MonoBehaviour {
+
+    public float mouseSensitivity = 10;
+    public Transform target;
+    public float dstFromTarget = 2;
+
+    public float rotationSmoothTime = 1.2f;
+    Vector3 rotationSmoothVelocity;
+    Vector3 currentRotation;
+
+    public Vector2 lookMinMax = new Vector2(-40, 85);
+
+    float mouseX;
+    float mouseY;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+        mouseX += Input.GetAxis("Mouse X") * mouseSensitivity;
+        mouseY -= Input.GetAxis("Mouse Y") * mouseSensitivity;
+
+        mouseY = Mathf.Clamp(mouseY, lookMinMax.x, lookMinMax.y);
+
+        currentRotation = Vector3.SmoothDamp(currentRotation, new Vector3(mouseY, mouseX), ref rotationSmoothVelocity, rotationSmoothTime);
+
+        transform.eulerAngles = currentRotation;
+
+        transform.position = target.position - transform.forward * dstFromTarget;
+	}
+}
